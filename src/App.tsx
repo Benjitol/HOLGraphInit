0075a0d3533e50ce621b51707979f92f57add423
Benjamin Tolaval <benjamin.tolaval@cellenza.com>
2019-05-13 10:48:51 +0200

    Mettre en place l'authent MSAL

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,77 @@
 import React, * as react from 'react';
 import './App.css';
+import { UserAgentApplication } from 'msal';
+import { appId, scopes } from './Config';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from './Components/NavBar';
 import { Container, Row, Col } from 'reactstrap';
 import SubmitForm from './Components/SubmitForm';
 
-class App extends react.Component {
+class App extends react.Component<{}, { isAuthenticated: boolean }> {
+  userAgentApplication: UserAgentApplication;
   submitForm: react.RefObject<SubmitForm>;
   constructor(props: any) {
     super(props);
+    var msalConfig = {
+      auth: {
+        clientId: appId
+      }
+    };
+    this.userAgentApplication = new UserAgentApplication(msalConfig);
+    var user = this.userAgentApplication.getAccount();
+    this.state = {
+      isAuthenticated: (user !== null)
+    };
+
+    if (user) {
+      this.LoadData()
+    }
     this.submitForm = React.createRef();
   }
   render() {
     return (
       <div>
-        <NavBar />
+        <NavBar isAuthenticated={this.state.isAuthenticated}
+          authButtonMethod={this.state.isAuthenticated ? this.logout.bind(this) : this.login.bind(this)}
+          user={{}} />
         <Container>
           <Row>
             <Col>
-              <SubmitForm  ref={this.submitForm} ></SubmitForm>
+              <SubmitForm ref={this.submitForm} ></SubmitForm>
             </Col>
           </Row>
         </Container>
       </div>
     );
+  }
+
+  async login() {
+    try {
+      var loginRequest = {
+        scopes: scopes
+      };
+      await this.userAgentApplication.loginPopup(loginRequest);
+      this.setState({
+        isAuthenticated: true,
+      });
+      //await this.getUserProfile();
+    }
+    catch (err) {
+      var errParts = err.split('|');
+      this.setState({
+        isAuthenticated: false,
+        //user: {},
+        //error: { message: errParts[1], debug: errParts[0] }
+      });
+    }
+  }
+
+  logout() {
+    this.userAgentApplication.logout();
+  }
+
+  private LoadData() {
+    //this.getUserProfile();
   }
 }
 
---
+++ src/Config.ts
@@ -0,0 +1,2 @@
+export const appId= '4259da76-eb44-4222-b3d7-e17a500d7986';
+export const scopes= ["User.Read"]//,"People.Read","Sites.Read.All","User.Read.All","Files.Read.All"];
